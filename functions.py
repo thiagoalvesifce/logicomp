3223b6c0540ac4ca1deb88570fab0b210b24a812
thiagoalvesifce <thiagoalvesifce@gmail.com>
2020-02-17 22:27:52 -0300

    fix indentation

--- functions.py
+++ functions.py
@@ -6,25 +6,25 @@ from formula import *
 
 
 def length(formula):
-	"""Determines the length of a formula in propositional logic."""
-	if isinstance(formula, Atom):
-		return 1
-	# ======== YOUR CODE HERE ========
+    """Determines the length of a formula in propositional logic."""
+    if isinstance(formula, Atom):
+        return 1
+    # ======== YOUR CODE HERE ========
 
 
 def subformulas(formula):
-	"""Returns the set of all subformulas of a formula."""
-	pass
-	# ======== YOUR CODE HERE ========
+    """Returns the set of all subformulas of a formula."""
+    pass
+    # ======== YOUR CODE HERE ========
 
 
 def atoms(formula):
-	"""Returns the set of all atoms occurring in a formula."""
-	pass
-	# ======== YOUR CODE HERE ========
+    """Returns the set of all atoms occurring in a formula."""
+    pass
+    # ======== YOUR CODE HERE ========
 
 
 def number_of_atoms(formula):
-	"""Returns the number of distinct atoms occurring in a formula."""
-	pass
-	# ======== YOUR CODE HERE ========
+    """Returns the number of distinct atoms occurring in a formula."""
+    pass
+    # ======== YOUR CODE HERE ========
