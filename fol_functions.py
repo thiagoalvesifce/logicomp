e8a540cb35d1e4fcd0ffb1bbbfbe3dc698b4bc05
thiagoalvesifce <thiagoalvesifce@gmail.com>
2021-03-01 04:17:54 -0300

    add functions signatures for first-order logic

---
+++ fol_functions.py
@@ -0,0 +1,86 @@
+from fol_formula import *
+from term import *
+
+
+def length_fol(formula):
+    """Determines the length of a formula in first-order logic."""
+    if isinstance(formula, Atom):
+        return 1
+    if isinstance(formula, Not):
+        return length_fol(formula.inner) + 1
+    if isinstance(formula, Implies) or isinstance(formula, And) or isinstance(formula, Or):
+        return length_fol(formula.left) + length_fol(formula.right) + 1
+    if isinstance(formula, ForAll) or isinstance(formula, Exists):
+        return 1 + length_fol(formula.inner)
+
+
+def subformulas_fol(formula):
+    """Returns the set of all subformulas of a first-order formula"""
+    pass
+
+
+def constants_from_term(term):
+    """Returns the set of all constant occurring in a term"""
+    pass
+
+
+def variables_from_term(term):
+    """Returns the set of all variables occurring in a term"""
+    pass
+
+
+def function_symbols_from_term(term):
+    """Returns the set of all function symbols occurring in a term
+    For example, function_symbols_from_term(Fun('f', [Var('x'), Con('a')]))
+    must return {'f'}
+
+    and
+
+    function_symbols_from_term(Fun('g', [Fun('f', [Var('x'), Con('a')])]))
+    must return {'f', 'g'}
+    """
+    pass
+
+
+def all_constants(formula):
+    """Returns the set of all constant occurring in a formula"""
+    pass
+
+
+def predicate_symbols(formula):
+    """Returns the set of all predicate symbols occurring in a formula.
+    For example, predicate_symbols(Or(Atom('P', [Con('a')]), Atom('R', [Var('x')])))
+    must return {'P', 'R'}
+    """
+    pass
+
+
+def function_symbols(formula):
+    """Returns the set of all function symbols occurring in a formula.
+    For example, predicate_symbols(Or(Atom('P', [Fun('f', [Con('b'), Var('y')])]),
+                                      Atom('P', [Fun('g', [Var('y')])])
+                                      )
+                                   )
+    must return {'f', 'g'}
+    """
+    pass
+
+
+def atoms_fol(formula):
+    """Returns the set of all atomic suformulas of a first-order formula"""
+    pass
+
+
+def free_variables(formula):
+    """Returns the set of all free variables of a formula"""
+    pass
+
+
+def universal_closure(formula):
+    """Returns the universal closure of a formula"""
+    pass
+
+
+def existential_closure(formula):
+    """Returns the existential closure of a formula"""
+    pass
